df653130df524a5106421a7810e487912ea36403
hendisantika <hendisantika@yahoo.co.id>
2024-03-25 12:23:41 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootdatacassandra/SpringBootDataCassandraApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootdatacassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataCassandraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataCassandraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootdatacassandra/SpringBootDataCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootdatacassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataCassandraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
