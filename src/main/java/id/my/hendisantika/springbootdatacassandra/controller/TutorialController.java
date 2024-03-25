ecc5da0e009eb9ec8d93fbd1e1d7d64e7403e073
hendisantika <hendisantika@yahoo.co.id>
2024-03-25 12:27:30 +0700

    Add getAllTutorials in TutorialController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootdatacassandra/controller/TutorialController.java
+++ src/main/java/id/my/hendisantika/springbootdatacassandra/controller/TutorialController.java
@@ -1,10 +1,18 @@
 package id.my.hendisantika.springbootdatacassandra.controller;
 
+import id.my.hendisantika.springbootdatacassandra.model.Tutorial;
 import id.my.hendisantika.springbootdatacassandra.repository.TutorialRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +31,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class TutorialController {
 
     private final TutorialRepository tutorialRepository;
+
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+        try {
+            List<Tutorial> tutorials = new ArrayList<>();
+
+            if (title == null)
+//                tutorialRepository.findAll().forEach(tutorials::add);
+                tutorials.addAll(tutorialRepository.findAll());
+            else
+//                tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+                tutorials.addAll(tutorialRepository.findByTitleContaining(title));
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
