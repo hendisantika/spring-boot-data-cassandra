39f09bf0e61d00c9cde62ba3d64925ff8ea741fb
hendisantika <hendisantika@yahoo.co.id>
2024-03-25 12:27:55 +0700

    Add getTutorialById in TutorialController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootdatacassandra/controller/TutorialController.java
+++ src/main/java/id/my/hendisantika/springbootdatacassandra/controller/TutorialController.java
@@ -7,12 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -52,5 +55,13 @@ public class TutorialController {
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") UUID id) {
+        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+
+//        if (tutorialData.isPresent()) {
+        return tutorialData.map(tutorial -> new ResponseEntity<>(tutorial, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
